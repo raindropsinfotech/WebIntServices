b4d93f45c2962f3e260a3fe1df5ee0d9ca70bd63
imranbajwa <imranbajwa01@gmail.com>
2024-02-17 17:22:43 +0100

    - Added table 'external_connection_mappings' table to setup mapping between different type of settings - MailSettings model and resource added to the project

---
+++ app/Nova/MailSetting.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class MailSetting extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\MailSetting>
+     */
+    public static $model = \App\Models\MailSetting::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make('id', 'Id')->sortable(),
+            Text::make('host', 'Host'),
+            Text::make('username', 'Username'),
+            Text::make('paswword', 'Password'),
+            Text::make('port', 'Port'),
+            Text::make('fromEmail', 'FromEmail'),
+            Text::make('ccEmail', 'CCEmail'),
+            Text::make('bccEmail', 'BCCEmail'),
+            Text::make('comment', 'Comment'),
+            Boolean::make('active', 'Acrtive')
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
